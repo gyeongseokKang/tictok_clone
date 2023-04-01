e157d94a536ba424cc2ca645051d72acfaa49e5d
handy <hk@gaudiolab.com>
2023-04-01 17:28:14 +0900

    feat : 기본 로그인 관련 스크린 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictok_clone/features/authentication/sign_up_screen.dart';
 
 void main() {
   runApp(const TiktokApp());
@@ -15,8 +16,6 @@ class TiktokApp extends StatelessWidget {
         theme: ThemeData(
           primaryColor: const Color(0xFFE9435A),
         ),
-        home: Container(
-          child: const Text("dddd"),
-        ));
+        home: const SignUpScreen());
   }
 }
